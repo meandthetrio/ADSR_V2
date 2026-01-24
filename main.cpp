cb89fb6e3bf1cea08601e21226aefdff18533a75
meandthetrio <kelliot985@gmail.com>
2026-01-23 18:48:34 -0600

    Initial ADSR_V2 Pod project skeleton

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "daisy_pod.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+DaisyPod hw;
+
+static void AudioCallback(AudioHandle::InputBuffer in,
+                          AudioHandle::OutputBuffer out,
+                          size_t size)
+{
+    for(size_t i = 0; i < size; i++)
+    {
+        // Simple passthrough
+        out[0][i] = in[0][i];
+        out[1][i] = in[1][i];
+    }
+}
+
+int main(void)
+{
+    hw.Init();
+    hw.SetAudioBlockSize(48);     // typical starting point
+    hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+
+    hw.StartAudio(AudioCallback);
+
+    while(1)
+    {
+        hw.DelayMs(10);
+    }
+}
